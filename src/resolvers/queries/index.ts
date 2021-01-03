7a5178624563b8317aecfe995a6e7d7e32d79d64
Victor Iris <victormidp@gmail.com>
2021-01-02 20:30:23 -0600

    Add best recipe query

--- src/resolvers/queries/index.ts
+++ src/resolvers/queries/index.ts
@@ -1,10 +1,26 @@
-import { extendType } from "nexus";
+import { extendType, nullable } from "nexus";
 
 export const queries = extendType({
-    type: "Query",
-    definition(t) {
-        t.crud.recipes();
-        t.crud.recipe();
-        t.crud.ingredients();
-    }
-})
+	type: "Query",
+	definition(t) {
+		t.crud.recipes();
+		t.crud.recipe();
+		t.crud.ingredients();
+		t.field("bestRecipe", {
+			type: nullable("Recipe"),
+			description: "Get the best rated recipe",
+			resolve: async (parent, args, ctx) => {
+				const [bestRecipe] = await ctx.prisma.$queryRaw(`
+                SELECT "recipeId", SUM(score) FROM "Rating" as r
+                GROUP BY "recipeId" LIMIT 1
+                `);
+
+				return ctx.prisma.recipe.findUnique({
+					where: {
+						id: bestRecipe.recipeId,
+					},
+				});
+			},
+		});
+	},
+});
